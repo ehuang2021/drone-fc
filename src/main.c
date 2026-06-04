ac258d64ee1eb4537b0fd68f19dc5fec045ce197
Eric Huang <erichuang@mac.mynetworksettings.com>
2026-06-04 00:36:23 -0400

    changed code to outputting in csv

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@ int main(void)
         static const struct i2c_dt_spec dev_i2c = I2C_DT_SPEC_GET(I2C_NODE);
         struct accelrometer_data acc;
         struct gyro_data gyro;
+        int counter = 0;
 
         while (1) {
                 int ret = init_sensor(&dev_i2c);
@@ -19,12 +20,10 @@ int main(void)
                 read_acclerometer(&dev_i2c, &acc);
                 read_gyro(&dev_i2c, &gyro);
 
-                printk("Accelerometer X: %d, Y: %d, Z: %d", acc.accel_x, acc.accel_y, acc.accel_z);
+                printk("%d,%d,%d,%d", counter, acc.accel_x, acc.accel_y, acc.accel_z);
+                printk("%d,%d,%d\n", gyro.accel_x, gyro.accel_y, gyro.accel_z);
+                counter++;
 
-                k_msleep(100);
-                printk("    |||     Gyro X: %d, Y: %d, Z: %d\n", gyro.accel_x, gyro.accel_y, gyro.accel_z);
-
-                k_msleep(1000);
         }
 
         return 0;
