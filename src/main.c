b346b4070b8847c741d90a641598c77d529544e8
Eric Huang <erichuang@mac.mynetworksettings.com>
2026-06-07 01:13:09 -0400

    Added initalize imu inturrupts

--- src/main.c
+++ src/main.c
@@ -1,30 +1,42 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/i2c.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/logging/log.h>
+
+
 #include "mpu6050.h"
+#include "imu.h"
 
-#define I2C_NODE DT_NODELABEL(mysensor)
+#define I2C_NODE DT_NODELABEL(mpu6050)
 
-int main(void)
-{
-        static const struct i2c_dt_spec dev_i2c = I2C_DT_SPEC_GET(I2C_NODE);
-        struct accelrometer_data acc;
-        struct gyro_data gyro;
-        int counter = 0;
+// Initalize GPIO
+static const struct gpio_dt_spec mpu_int_gpio = GPIO_DT_SPEC_GET(I2C_NODE, int_gpios);
+static struct gpio_callback mpu_int_cb_data;
 
-        while (1) {
-                int ret = init_sensor(&dev_i2c);
-                if (!ret) {
-                        return -1;
-                }
+LOG_MODULE_REGISTER(main_task, LOG_LEVEL_DBG);
 
-                read_acclerometer(&dev_i2c, &acc);
-                read_gyro(&dev_i2c, &gyro);
 
-                printk("%d,%d,%d,%d", counter, acc.accel_x, acc.accel_y, acc.accel_z);
-                printk("%d,%d,%d\n", gyro.accel_x, gyro.accel_y, gyro.accel_z);
-                counter++;
+// This function init's the imu inturupt, which is responsible for thread unblocking for imu.c
+int initalize_imu_inturupts() {
+    int ret = gpio_pin_configure_dt(&mpu_int_gpio, GPIO_INPUT);
+    if (ret) {
+            LOG_ERR("gpio_pin_configure_dt Error code: %d", ret);
+            return ret;
+    }
 
-        }
+    ret = gpio_pin_interrupt_configure_dt(&mpu_int_gpio, GPIO_INT_EDGE_TO_ACTIVE);
+    if (ret) {
+            LOG_ERR("gpio_pin_interrupt_configure_dt error: Error code: %d", ret);
+            return ret;
+    }
+    gpio_init_callback(&mpu_int_gpio, mpu_inturrupt, BIT(mpu_int_gpio.pin)); 	
+    gpio_add_callback(mpu_int_gpio.port, &mpu_int_cb_data);
+    
+    return 0;
 
-        return 0;
+
+}
+int main() {
+    initalize_imu_inturupts();
+    return 0;
 }
