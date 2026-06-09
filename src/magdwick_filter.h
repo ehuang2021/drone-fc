0376f09bab9e178dc1e6f8bb50c0e4dd7da410cb
Eric Huang <erichuang@mac.mynetworksettings.com>
2026-06-08 20:04:19 -0400

    Refactored the filter to signal precision floats and replaced 64 bit double math to 32 bit to run on the nRF62832 FPU

---
+++ src/magdwick_filter.h
@@ -0,0 +1,14 @@
+#ifndef MAGDWICK_FILTER_H
+#define MAGDWICK_FILTER_H
+
+typedef struct {
+    float q1;
+    float q2;
+    float q3;
+    float q4;
+} Quanterion;
+
+void filterUpdate(float w_x, float w_y, float w_z, float a_x, float a_y, float a_z, Quanterion *q, float delta);
+
+
+#endif
